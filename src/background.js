c17c39c7394157d5b9d8a63e8a39854bd26912fe
Scott MacLellan <s.j.maclell@gmail.com>
2015-10-03 01:24:23 -0400

    Using a local instead site

--- src/background.js
+++ src/background.js
@@ -1,3 +1,5 @@
+var insteadUrl = chrome.extension.getURL( "instead.html" );
+
 function shouldBlockUrl( url ) {
 	var hasLol = url.indexOf( "lol" ) != -1;
   var alreadyRedirected = url.indexOf( "?site=" ) != -1;
@@ -9,7 +11,7 @@ chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
     var block = shouldBlockUrl( details.url );
     if( block ) {
-      return { redirectUrl: "http://google.com/?site="+details.url};
+      return { redirectUrl: insteadUrl + "?site=" + details.url };
     } else {
       return {};
     }
